88cd86d377e7480d1f2b781bb48b89a12d039d1b
melissa rice <rice.jean29@gmail.com>
2018-05-07 18:35:02 -0600

    version10

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,10 +3,6 @@ var path = require("path");
 
 module.exports = (app) => {
 
-	// app.get('/', (req, res) => {
-	// 	// res.sendFile(path.join(__dirname, 'home.html'));
-	// 	res.sendFile('home.html', { root: './app/public' });
-	// });
 
 	app.get('/', (req, res) => {
 		res.sendFile((path.join(__dirname, '../public/home.html')))
@@ -15,11 +11,7 @@ module.exports = (app) => {
 
 	app.get('/survey', (req,res) => {
 		// res.sendFile(path.join(__dirname, 'survey.html'));
-		res.sendFile('survey.html', { root: './app/public' });
+		res.sendFile((path.join(__dirname, '../public/survey.html')));
 	})
 
-	// app.get('/friends.js', (req,res) => {
-	// 	// res.sendFile(path.join(__dirname, 'survey.html'));
-	// 	res.sendFile('friends.js', { root: './' });
-	// })
 };
